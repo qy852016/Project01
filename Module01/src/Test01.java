f8feda873cfe78b10dc7c39b2edf11d9e31b7ead
qy <932462697@qq.com>
2020-03-05 21:39:40 +0800

    Initial commit

---
+++ Module01/src/Test01.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Test01 {
+    public static void main (String[] args){
+        System.out.println(123);
+    }
+
+    public void test(){
+        HashMap map = new HashMap();
+    }
+}
